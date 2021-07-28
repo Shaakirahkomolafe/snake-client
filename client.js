c71f213fdb867d1b33d7d26b901ca02e418b5f6c
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-07-28 18:38:29 -0400

    new changes due to input module

--- client.js
+++ client.js
@@ -18,28 +18,11 @@ const connect = function() {
 
   return conn;
 };
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-      
-    });
-  };
-  
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+
 
 console.log("Connecting ...");
 connect();
-setupInput();
+
 module.exports = {
   connect,
 };
--- play.js
+++ play.js
@@ -1,4 +1,11 @@
 const { connect } = require("./client.js");
+const { setupInput } = require("./input");
+
+console.log("Connecting ...");
+connect();
+
+setupInput();
+
 
 
 // establishes a connection with the game server
