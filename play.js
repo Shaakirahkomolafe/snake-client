8dc8fce65d9bc90f156b0311060156e40dd0fd9c
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-07-28 15:20:39 -0400

    connection module

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require("net");
+const { connect } = require("./client.js");
+
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.223.133',// IP address here,
-    port:50542, // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) =>{
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
